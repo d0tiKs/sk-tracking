56ce836e91ad569ed5f7fda8e9b490507de74f84
raven <raven@raven-systems.space>
2025-08-07 22:17:40 +0200

    fix vite packaging

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  server: {
-    port: 5174
-  },
+  base: '/sk-tracking/',
   plugins: [
     react(),
     VitePWA({
